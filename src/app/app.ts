7423a82921b8ac6834acb9cd7ed2f5548dedd23b
Manou Dierckx <r0231646@student.thomasmore.be>
2025-09-26 15:07:06 +0200

    about us - beetje styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BuildSetComponent } from './build-set-component/build-set-component';
+import { AboutUsComponent } from './about-us-component/about-us-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BuildSetComponent],
+  imports: [RouterOutlet, BuildSetComponent, AboutUsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
